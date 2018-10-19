a0007236459d133d372d1e009ca152a0c848e177
andreasonny83 <andreasonny83@gmail.com>
2018-10-19 15:00:43 +0100

    :white_check_mark: Add create-flex-plugin tests

--- packages/create-flex-plugin/__tests__/create-flex-plugin.test.js
+++ packages/create-flex-plugin/__tests__/create-flex-plugin.test.js
@@ -1,7 +1,104 @@
 import createFlexPlugin from '../src/create-flex-plugin';
+import inquirer from 'inquirer';
+import execa from 'execa';
+import * as log from '../src/logging';
+
+jest.mock('inquirer');
+jest.mock('execa');
+jest.mock('../src/logging');
 
 describe('create-flex-plugin', () => {
-    it('needs tests', () => {
-        expect(true).toBeTruthy();
+  beforeEach(() => jest.clearAllMocks());
+
+  test(`a 'plug-in' name should be specified`, async () => {
+    // Arrange
+    const spyExit = jest.spyOn(process, 'exit').mockImplementation(() => {});
+    const config = {};
+
+    // Act
+    await createFlexPlugin(config);
+
+    // Assert
+    expect(spyExit).toHaveBeenCalledWith(1);
+  });
+
+  test(`should only accept plugin names starting with 'plugin-'`, async() => {
+    // Act
+    await createFlexPlugin({name: 'my-plugin'});
+
+    // Assert
+    expect(log.error).toHaveBeenCalledWith('Invalid plugin name. Names need to start with plugin-');
+  });
+
+  test(`should ask for an accountSid if not specified`, async() => {
+    // Arrange
+    inquirer.prompt = jest.fn(() => Promise.resolve({
+      accountSid: 'test-sid'
+    }));
+
+    // Act
+    await createFlexPlugin({
+      name: 'plugin-test',
+      adminPlugin: true
     });
+
+    // Assert
+    expect(inquirer.prompt).toHaveBeenCalledTimes(1);
+  });
+
+  test(`should not ask for an accountSid if already specified`, async() => {
+    // Arrange
+    inquirer.prompt = jest.fn();
+
+    // Act
+    await createFlexPlugin({
+      name: 'plugin-test',
+      accountSid: 'fake-sid',
+      adminPlugin: true
+    });
+
+    // Assert
+    expect(inquirer.prompt).not.toHaveBeenCalled();
+  });
+
+  test(`should ask fro including an admin plugin if not specified`, async() => {
+    // Arrange
+    inquirer.prompt = jest.fn(() => Promise.resolve({
+      adminPlugin: false
+    }));
+
+    // Act
+    await createFlexPlugin({
+      name: 'plugin-test',
+      accountSid: 'fake-sid'
+    });
+
+    // Assert
+    expect(inquirer.prompt).toHaveBeenCalledTimes(1);
+  });
+
+  test(`should not install any dependency by default`, async() => {
+    // Act
+    await createFlexPlugin({
+      name: 'plugin-test',
+      accountSid: 'fake-sid',
+      adminPlugin: true
+    });
+
+    // Assert
+    expect(execa).not.toHaveBeenCalled();
+  });
+
+  test(`should install the dependencies if specified`, async() => {
+    // Act
+    await createFlexPlugin({
+      name: 'plugin-test',
+      accountSid: 'fake-sid',
+      adminPlugin: true,
+      install: true
+    });
+
+    // Assert
+    expect(execa).toHaveBeenCalled();
+  });
 });
