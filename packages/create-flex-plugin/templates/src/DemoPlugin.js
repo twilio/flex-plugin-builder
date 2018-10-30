f6c683dfaf9abb425bb63be06b5d892457fbbff3
Dominik Kundel <dominik.kundel@gmail.com>
2018-10-30 15:50:10 +0100

    :sparkles: Add jsDoc comments and fix typings

--- packages/create-flex-plugin/templates/src/DemoPlugin.js
+++ packages/create-flex-plugin/templates/src/DemoPlugin.js
@@ -2,12 +2,26 @@ import { FlexPlugin } from 'flex-plugin';
 import React from 'react';
 import CustomTaskListComponent from './CustomTaskListComponent';
 
-export default class {{pluginClassName}} extends FlexPlugin {
-  name = '{{pluginClassName}}';
+const PLUGIN_NAME = '{{pluginClassName}}';
 
+export default class {{pluginClassName}} extends FlexPlugin {
+  constructor() {
+    super(PLUGIN_NAME);
+  }
+
+  /**
+   * This code is run when your plugin is being started
+   * Use this to modify any UI components or attach to the actions framework
+   *
+   * @param flex { typeof import('@twilio/flex-ui') }
+   * @param manager { import('@twilio/flex-ui').Manager }
+   */
   init(flex, manager) {
-    flex.AgentDesktopView.Panel1.Content.add(<CustomTaskListComponent key="demo-component" />, {
-      sortOrder: -1,
-    });
+    flex.AgentDesktopView.Panel1.Content.add(
+      <CustomTaskListComponent key="demo-component" />,
+      {
+        sortOrder: -1,
+      }
+    );
   }
 }
