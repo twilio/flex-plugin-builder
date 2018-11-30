30b3db1894dd1006be59b0cfabc436a305955cf3
andreazornada <azornada@twilio.com>
2018-11-30 18:17:52 +0000

    Fix login redirect issue

--- packages/flex-plugin/dev_assets/flex.js
+++ packages/flex-plugin/dev_assets/flex.js
@@ -2,6 +2,10 @@ const REDIRECT_KEY = 'LOCAL_FLEX_PLUGIN_LOGIN_REDIRECT';
 const FIRST_LOAD_KEY = 'LOCAL_FLEX_PLUGIN_FIRST_LOAD';
 
 function loadFlex() {
+  const localPortQuery = window.location.port
+    ? '?localPort=${window.location.port}'
+    : '';
+
   if (
     typeof appConfig === 'undefined' ||
     !appConfig.sso.accountSid ||
@@ -11,17 +15,21 @@ function loadFlex() {
       'ERROR: You must have a valid appConfig with both accountSid and serviceBaseUrl set.'
     );
   } else {
-    const loginUrl = `http://www.twilio.com/service-login/flex/${appConfig.sso.accountSid || accountSid}?localPort=${window.location.port}`
+    const baseUrl = 'http://www.twilio.com/service-login/flex';
+    const loginUrl = `${baseUrl}/${appConfig.sso.accountSid || accountSid}${localPortQuery}`;
+
     if (!localStorage.getItem(FIRST_LOAD_KEY)) {
       localStorage.setItem(FIRST_LOAD_KEY, 'redirected');
       window.location.replace(loginUrl);
       return;
-    } else {
-      Twilio.Flex.create(appConfig).then(flex => {
+    }
+
+    Twilio.Flex.create(appConfig)
+      .then(flex => {
         localStorage.removeItem(REDIRECT_KEY);
-        localStorage.removeItem(FIRST_LOAD_KEY);
         flex.init('#container');
-      }).catch(err => {
+      })
+      .catch(err => {
         if (!localStorage.getItem(REDIRECT_KEY)) {
           localStorage.setItem(REDIRECT_KEY, 'redirected');
           window.location.replace(loginUrl);
@@ -29,7 +37,5 @@ function loadFlex() {
           throw new Error('Failed to authenticate user.');
         }
       });
-    }
-
   }
 }
