058db0da2ac7ade698edf5a649c30cdc2653e315
andreazornada <azornada@twilio.com>
2018-12-18 17:50:16 +0000

    :wrench: Remove deprecated serviceBaseUrl and tokenizerUrl from appConfig

--- packages/flex-plugin/dev_assets/flex.js
+++ packages/flex-plugin/dev_assets/flex.js
@@ -1,13 +1,13 @@
 (function(global, undefined) {
   'use strict';
   global.loadFlex = function loadFlex(targetEl) {
-    if (
-      typeof appConfig === 'undefined' ||
-      !(appConfig.sso && appConfig.sso.accountSid) ||
-      !appConfig.serviceBaseUrl
+    if (!(
+      typeof appConfig !== 'undefined' &&
+      appConfig.sso &&
+      appConfig.sso.accountSid)
     ) {
       console.error(
-        'ERROR: You must have a valid appConfig with both accountSid and serviceBaseUrl set.'
+        'ERROR: You must have a valid appConfig with an accountSid set.'
       );
     } else {
       Twilio.Flex.runDefault(appConfig, targetEl).catch(() => {
