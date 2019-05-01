3f408d5c7b14f9597735992fd4d2fb47a0ceff7e
Kousha Talebian <kousha.talebian@gmail.com>
2019-05-01 12:26:07 -0700

    address pr comments

--- packages/create-flex-plugin/templates/craco.config.js
+++ packages/create-flex-plugin/templates/craco.config.js
@@ -3,10 +3,10 @@ const config = require('craco-config-flex-plugin');
 module.exports = {
   ...config,
   plugins: [
-    // Customize app configuration (such as webpack, devServer, linter, etc) by creating a craco plugin.
+    // Customize app configuration (such as webpack, devServer, linter, etc) by creating a Craco plugin.
     // See https://github.com/sharegate/craco/tree/master/packages/craco#develop-a-plugin for more detail.
     //
-    // Please note that craco plugins have nothing to do with flex plugins, just a naming coincidence.
+    // Please note that Craco plugins have nothing to do with Flex plugins; it's just a naming coincidence.
     // Changes to this file are optional, you will not need to modify it for normal Flex Plugin development.
   ]
 };
