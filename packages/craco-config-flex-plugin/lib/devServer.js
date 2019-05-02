8be534e1aad7d0c02b5826664595e22a89e3d86b
Kousha Talebian <ktalebian@twilio.com>
2019-05-01 17:41:37 -0700

    only output bundle and map

--- packages/craco-config-flex-plugin/lib/devServer.js
+++ packages/craco-config-flex-plugin/lib/devServer.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = (config, { env, paths, proxy, allowedHost }) => {
-     config.contentBase = [
-          config.contentBase,
-          path.join(process.cwd(), 'node_modules', 'flex-plugin', 'dev_assets'),
-     ];
+  config.contentBase = [
+    config.contentBase,
+    path.join(process.cwd(), 'node_modules', 'flex-plugin', 'dev_assets'),
+  ];
 
-     return config;
+  return config;
 };
