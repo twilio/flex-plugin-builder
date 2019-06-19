9b8bab988e48af7e3d31ef5509da402e75d657df
Kousha Talebian <kousha.talebian@gmail.com>
2019-06-18 18:12:28 -0700

    move all fs files to dev-utils

--- packages/flex-dev-utils/src/fs.ts
+++ packages/flex-dev-utils/src/fs.ts
@@ -4,10 +4,36 @@ import * as path from 'path';
 export default fs;
 
 /**
+ * Checks the provided array of files exist
+ *
+ * @param files the files to check that they exist
+ */
+export const checkFilesExist = (...files: string[]) => {
+  return files
+    .map(fs.existsSync)
+    .every((resp) => resp);
+};
+
+/**
+ * Gets package.json path
+ */
+export const getPackageJsonPath = () => path.join(process.cwd(), 'package.json');
+
+/**
+ * Updates the package.json version field
+ *
+ * @param version the new version
+ */
+export const updatePackageVersion = (version: string) => {
+  const packageJson = readPackageJson();
+  packageJson.version = version;
+
+  fs.writeFileSync(getPackageJsonPath(), JSON.stringify(packageJson, null, 2));
+};
+
+/**
  * Reads package.json
  */
 export const readPackageJson = () => {
-  const packageJsonPath = path.join(process.cwd(), 'package.json');
-
-  return JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+  return JSON.parse(fs.readFileSync(getPackageJsonPath(), 'utf8'));
 };
