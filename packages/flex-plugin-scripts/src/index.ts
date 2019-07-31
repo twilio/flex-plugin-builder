88485719dd7707fa52d763b32352bbf43b4157e2
Kousha Talebian <kousha.talebian@gmail.com>
2019-07-31 11:42:18 -0700

    add main test

--- packages/flex-plugin-scripts/src/index.ts
+++ packages/flex-plugin-scripts/src/index.ts
@@ -14,22 +14,26 @@ checkForUpdate();
 const spawnScript = (...argv: string[]) => {
   // Directory of this file
   const dir = dirname(__filename);
-  console.log(argv);
 
   // Get all the scripts inside /scripts directory
   // `run.js` is an exception, so filter that one out
   const files = readdirSync(join(dir, 'scripts'));
   const scripts = files
-    .filter((f) => f.split('.').pop() === 'js')
+    .filter((f) => {
+      const ext = f.split('.').pop();
+
+      return ext === 'js' || ext === 'ts';
+    })
     .map((f) => f.split('.')[0])
     .filter((f) => f !== 'run');
 
   const scriptIndex = argv.findIndex((x) => scripts.includes(x));
   const script = scriptIndex !== -1 && argv[scriptIndex];
+
   if (!script) {
     const options = logger.colors.blue(scripts.join(', '));
     logger.error(`Unknown script '${script}'; please choose from one of: ${options}.`);
-    process.exit(1);
+    return process.exit(1);
   }
 
 // Print help doc and quit
@@ -37,11 +41,11 @@ const spawnScript = (...argv: string[]) => {
     const docPath = join(dir, '../docs', script) + '.md';
     if (!existsSync(docPath)) {
       logger.warning(`No documentation was found for ${script}`);
-      process.exit(1);
+      return process.exit(1);
     }
 
     markedRender(docPath);
-    process.exit(0);
+    return process.exit(0);
   }
 
   const nodeArgs = scriptIndex > 0 ? argv.slice(0, scriptIndex) : [];
@@ -53,7 +57,7 @@ const spawnScript = (...argv: string[]) => {
   processArgs.push('--disallow-versioning');
 
   // Run the script and then exit
-  process.exit(spawn(processArgs));
+  return process.exit(spawn(processArgs));
 };
 
 export default spawnScript;
