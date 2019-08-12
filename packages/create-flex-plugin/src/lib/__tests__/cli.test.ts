943386b2e759d6196a7e17b99da6239691bec38f
Kousha Talebian <kousha.talebian@gmail.com>
2019-08-12 13:41:36 -0700

    lint tests

--- packages/create-flex-plugin/src/lib/__tests__/cli.test.ts
+++ packages/create-flex-plugin/src/lib/__tests__/cli.test.ts
@@ -4,9 +4,9 @@ import createFlexPlugin from '../create-flex-plugin';
 jest.mock('../create-flex-plugin');
 
 describe('cli', () => {
-    it('should call createFlexPlugin', () => {
-        cli().parse();
+  it('should call createFlexPlugin', () => {
+    cli().parse();
 
-        expect(createFlexPlugin).toHaveBeenCalled();
-    });
+    expect(createFlexPlugin).toHaveBeenCalled();
+  });
 });
--- packages/create-flex-plugin/src/lib/__tests__/create-flex-plugin.test.ts
+++ packages/create-flex-plugin/src/lib/__tests__/create-flex-plugin.test.ts
@@ -7,41 +7,40 @@ import createFlexPlugin from '../create-flex-plugin';
 jest.mock('flex-dev-utils/dist/logger');
 
 describe('create-flex-plugin', () => {
-    const accountSid = 'AC00000000000000000000000000000000';
-    const pluginName = 'plugin-test';
+  const accountSid = 'AC00000000000000000000000000000000';
+  const pluginName = 'plugin-test';
 
-    beforeEach(() => {
-        jest.clearAllMocks();
+  const clearDir = () => {
+    if (fs.existsSync(pluginName)) {
+      rimRaf.sync(pluginName);
+    }
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterEach(clearDir);
+  afterAll(clearDir);
+
+  describe('createFlexPlugin', () => {
+    it('should not install any dependency by default', async () => {
+      await createFlexPlugin({
+        name: pluginName,
+        accountSid,
+      } as any);
+
+      expect(execa).not.toHaveBeenCalled();
     });
 
-    afterEach(() => {
-        if (fs.existsSync(pluginName)) {
-            rimRaf.sync(pluginName);
-        }
+    it('should install the dependencies if specified', async () => {
+      await createFlexPlugin({
+        name: pluginName,
+        accountSid,
+        install: true,
+      } as any);
+
+      expect(execa).toHaveBeenCalled();
     });
-
-    describe('createFlexPlugin', () => {
-        it('should not install any dependency by default', async () => {
-            // Act
-            await createFlexPlugin({
-               name: pluginName,
-               accountSid,
-            } as any);
-
-            // Assert
-            expect(execa).not.toHaveBeenCalled();
-        });
-
-        it('should install the dependencies if specified', async () => {
-            // Act
-            await createFlexPlugin({
-                name: pluginName,
-                accountSid,
-                install: true,
-            } as any);
-
-            // Assert
-            expect(execa).toHaveBeenCalled();
-        });
-    });
+  });
 });
