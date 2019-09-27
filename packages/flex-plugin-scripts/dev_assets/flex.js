490c525f39e5da32e063393d9762096d3edb7dc3
Kousha Talebian <kousha.talebian@gmail.com>
2019-09-27 14:50:14 -0700

    update the flex.js shim

--- packages/flex-plugin-scripts/dev_assets/flex.js
+++ packages/flex-plugin-scripts/dev_assets/flex.js
@@ -1,19 +1,8 @@
 (function(global, undefined) {
   'use strict';
   global.loadFlex = function loadFlex(targetEl) {
-    const isAccountSidSet =
-      typeof appConfig !== 'undefined' &&
-      appConfig.sso &&
-      appConfig.sso.accountSid;
-
-    if (!isAccountSidSet) {
-      console.error(
-        'ERROR: You must have a valid appConfig with an accountSid set.'
-      );
-    } else {
-      Twilio.Flex.runDefault(appConfig, targetEl).catch(() => {
-        throw new Error('Failed to authenticate user.');
-      });
-    }
+    Twilio.Flex.runDefault(appConfig, targetEl).catch(() => {
+      throw new Error('Failed to authenticate user.');
+    });
   };
 })(typeof window !== 'undefined' ? window : this);
