bc9fce61d0466d12dd0d5f0a6cbf826ced4c7601
Kousha Talebian <kousha.talebian@gmail.com>
2020-04-01 11:15:59 -0700

    allow template url to work without template dir as well

--- packages/create-flex-plugin/src/utils/github.ts
+++ packages/create-flex-plugin/src/utils/github.ts
@@ -2,6 +2,7 @@ import axios, { AxiosRequestConfig } from 'flex-dev-utils/dist/axios';
 import { mkdirpSync } from 'flex-dev-utils/dist/fs';
 import fs from 'fs';
 import path from 'path';
+import has = Reflect.has;
 
 export interface GitHubInfo {
   owner: string;
@@ -59,9 +60,41 @@ export const parseGitHubUrl = (url: string): GitHubInfo => {
  * @return null
  */
 export const downloadRepo = async (info: GitHubInfo, dir: string) => {
-  const url = `https://api.github.com/repos/${info.owner}/${info.repo}/contents/template?ref=${info.ref}`;
+  const hasTemplateDir = await _hasTemplateDir(info);
 
-  return _downloadDir(url, dir);
+  const url = hasTemplateDir
+    ? `https://api.github.com/repos/${info.owner}/${info.repo}/contents/template?ref=${info.ref}`
+    : `https://api.github.com/repos/${info.owner}/${info.repo}/contents?ref=${info.ref}`;
+
+  return _downloadDir(url, dir, _getBaseRegex(info, hasTemplateDir));
+};
+
+/**
+ * Generates a regex for matching the download url
+ * @param info {GitHubInfo} the GitHub information
+ * @param hasTemplateDir {boolean} whether the GitHub repo has a template directory
+ * @private
+ */
+export const _getBaseRegex = (info: GitHubInfo, hasTemplateDir: boolean) => {
+  const baseRegex = `\/${info.owner}\/${info.repo}\/${info.ref}\/`;
+  if (hasTemplateDir) {
+    return baseRegex + 'template\/';
+  }
+
+  return baseRegex;
+};
+
+/**
+ * Checks for back-ward compatible changes; returns true is the repo has a template/ directory
+ * @param info {GitHubInfo}  the {@link GitHubInfo} information
+ * @private
+ */
+export const _hasTemplateDir = async (info: GitHubInfo) => {
+  const url = `https://api.github.com/repos/${info.owner}/${info.repo}/contents?ref=${info.ref}`;
+
+  return axios.get<GitHubContent[]>(url)
+    .then((resp) => resp.data)
+    .then(contents => contents.some(content => content.name === 'template' && content.type === 'dir'));
 };
 
 /**
@@ -69,22 +102,24 @@ export const downloadRepo = async (info: GitHubInfo, dir: string) => {
  *
  * @param url {string}  the url to download
  * @param dir {string}  the path to the directory to save the content
+ * @param baseRegex {String} the base path regex
  * @private
  */
-export const _downloadDir = async (url: string, dir: string): Promise<null> => {
+export const _downloadDir = async (url: string, dir: string, baseRegex: string): Promise<null> => {
   return axios.get<GitHubContent[]>(url)
     .then((resp) => resp.data)
     .then((contents) => {
       const promises = contents.map((content) => {
         if (content.type === GitHubContentType.Dir) {
-          return _downloadDir(content.url, dir);
+          return _downloadDir(content.url, dir, baseRegex);
         }
 
         if (content.type !== GitHubContentType.File) {
           throw new Error(`Unexpected content type ${content.type}`);
         }
 
-        const relativePath = content.download_url.match(/\/template\/(.+)\??/);
+        const regex = new RegExp(baseRegex + '(.+)\\??');
+        const relativePath = content.download_url.match(regex);
         if (!relativePath || relativePath.length !== 2) {
           throw new Error('Received invalid URL template');
         }
