94ecc52f2df4725d3482cad799fc7599917538db
Alice Cai <ahcai@berkeley.edu>
2020-04-02 11:00:32 -0700

    fix lint errors

--- packages/create-flex-plugin/src/utils/__tests__/github.test.ts
+++ packages/create-flex-plugin/src/utils/__tests__/github.test.ts
@@ -104,10 +104,10 @@ describe('github', () => {
   describe('_hasTemplateDir', () => {
     it('should return true if there is a template dir', async () => {
       const resp = [{
-        name: 'foo'
+        name: 'foo',
       }, {
         name: 'template',
-        type: 'dir'
+        type: 'dir',
       }];
 
       mockAxios.onGet().reply(() => Promise.resolve([200, resp]));
@@ -117,10 +117,10 @@ describe('github', () => {
 
     it('should return false if there is a template file', async () => {
       const resp = [{
-        name: 'foo'
+        name: 'foo',
       }, {
         name: 'template',
-        type: 'file'
+        type: 'file',
       }];
 
       mockAxios.onGet().reply(() => Promise.resolve([200, resp]));
@@ -130,10 +130,10 @@ describe('github', () => {
 
     it('should return false if there is no template dir', async () => {
       const resp = [{
-        name: 'foo'
+        name: 'foo',
       }, {
         name: 'something-else',
-        type: 'dir'
+        type: 'dir',
       }];
 
       mockAxios.onGet().reply(() => Promise.resolve([200, resp]));
