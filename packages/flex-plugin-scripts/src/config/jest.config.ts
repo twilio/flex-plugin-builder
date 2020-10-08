a610519d2699cfeecf8134de0d24f289ec73d8a3
Kousha Talebian <kousha.talebian@gmail.com>
2020-10-08 09:26:10 -0600

    address pr comment

--- packages/flex-plugin-scripts/src/config/jest.config.ts
+++ packages/flex-plugin-scripts/src/config/jest.config.ts
@@ -1,9 +1,7 @@
 import { InitialOptions } from '@jest/types/build/Config';
 import { getPaths } from 'flex-dev-utils/dist/fs';
 
-export {
-  JestConfigurations
-} from 'flex-plugin-test';
+export { JestConfigurations } from 'flex-plugin-test';
 
 /**
  * Main method for generating a default Jest configuration
