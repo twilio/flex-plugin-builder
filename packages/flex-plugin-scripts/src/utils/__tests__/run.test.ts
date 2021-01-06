89da0cf9f8d9b6c502086051c4695d970e12e704
Kousha Talebian <kousha.talebian@gmail.com>
2021-01-06 12:14:01 -0800

    remove unused tests

--- packages/flex-plugin-scripts/src/utils/__tests__/run.test.ts
+++ packages/flex-plugin-scripts/src/utils/__tests__/run.test.ts
@@ -15,60 +15,6 @@ describe('run', () => {
     process.env = { ...OLD_ENV };
   });
 
-  it('should run successfully', async () => {
-    const cb = jest.fn();
-    await run.default(cb);
-
-    expect(cb).toHaveBeenCalledTimes(1);
-  });
-
-  it('should log normal error if not a FlexPluginError', async () => {
-    const err = new Error('error-message');
-    const cb = jest.fn().mockImplementation(() => {
-      throw err;
-    });
-
-    await run.default(cb);
-
-    expect(exit).toHaveBeenCalledTimes(1);
-    expect(exit).toHaveBeenCalledWith(1);
-    expect(logger.error).toHaveBeenCalledTimes(1);
-    expect(logger.error).toHaveBeenCalledWith(err);
-  });
-
-  it('should log FlexPluginError print', async () => {
-    const err = new FlexPluginError('another-error');
-    const print = jest.spyOn(err, 'print');
-    const details = jest.spyOn(err, 'details');
-    const cb = jest.fn().mockImplementation(() => {
-      throw err;
-    });
-
-    await run.default(cb);
-
-    expect(exit).toHaveBeenCalledTimes(1);
-    expect(exit).toHaveBeenCalledWith(1);
-    expect(print).toHaveBeenCalledTimes(1);
-    expect(details).not.toHaveBeenCalled();
-  });
-
-  it('should also log details of FlexPluginError', async () => {
-    const err = new FlexPluginError('another-error');
-    const print = jest.spyOn(err, 'print');
-    const details = jest.spyOn(err, 'details');
-    const cb = jest.fn().mockImplementation(() => {
-      throw err;
-    });
-    process.env.DEBUG = 'true';
-
-    await run.default(cb);
-
-    expect(exit).toHaveBeenCalledTimes(1);
-    expect(exit).toHaveBeenCalledWith(1);
-    expect(print).toHaveBeenCalledTimes(1);
-    expect(details).toHaveBeenCalledTimes(1);
-  });
-
   describe('exit', () => {
     it('should not quit if arg has the flag', () => {
       run.exit(123, ['--no-process-exit']);
