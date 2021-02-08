052d6261c74452185dd701a1cf979d6a3b3a9b7e
Kousha Talebian <kousha.talebian@gmail.com>
2021-02-08 14:48:14 -0800

    support for nightly builds

---
+++ bin/nightly.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Does a nightly build and publishes to npm
+
+set -e
+
+lerna="./node_modules/.bin/lerna"
+
+function getJsonValue() {
+  KEY=$1
+  num=$2
+  awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p
+}
+
+currentVersion=$(cat ./lerna.json | getJsonValue 'version' |  cut -f1 -d"-")
+semver=( ${currentVersion//./ } )
+major="${semver[0]}"
+minor="${semver[1]}"
+patch="${semver[2]}"
+id=`echo $(date '+%Y%m%d')`
+nightlyVersion="${major}.${minor}.${patch}-nightly.${id}"
+echo $nightlyVersion
+
+npm run build
+${lerna} publish --force-publish="*" --skip-git --no-git-tag-version --no-push --yes --pre-dist-tag nightly "${nightlyVersion}"
+
+
