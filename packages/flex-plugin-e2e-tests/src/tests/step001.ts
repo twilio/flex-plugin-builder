73686a85758507b5ba250059efa54d7e282658c6
Kousha Talebian <kousha.talebian@gmail.com>
2021-04-19 15:41:20 -0700

    setup e2e test-job

--- packages/flex-plugin-e2e-tests/src/tests/step001.ts
+++ packages/flex-plugin-e2e-tests/src/tests/step001.ts
@@ -1,8 +1,9 @@
-/* eslint-disable import/no-unused-modules */
+/* eslint-disable import/no-unused-modules, no-console */
 import { TestSuite, TestParams } from '..';
 
 const testSuite: TestSuite = async (params: TestParams): Promise<void> => {
   /* Sample test */
+  console.log(process.env);
 };
 
 export default testSuite;
