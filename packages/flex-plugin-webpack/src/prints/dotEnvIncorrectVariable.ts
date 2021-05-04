ae553d4b624ef53f78c1aa0d6377458ef2562053
Kousha Talebian <kousha.talebian@gmail.com>
2021-05-04 11:06:20 -0700

    expose the env variables

--- packages/flex-plugin-webpack/src/prints/dotEnvIncorrectVariable.ts
+++ packages/flex-plugin-webpack/src/prints/dotEnvIncorrectVariable.ts
@@ -3,7 +3,9 @@ import { logger, env } from 'flex-dev-utils';
 export default (filename: string, key: string): void => {
   env.persistTerminal();
 
+  env.setQuiet(false);
   logger.warning(
     `Unsupported variable **${key}** provided in **${filename}** file. Variables must start with either FLEX_APP_ or REACT_APP_.`,
   );
+  env.setQuiet(true);
 };
