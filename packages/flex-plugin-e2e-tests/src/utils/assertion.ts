268e4acba46da5e581e9d376823b08c11a554cc1
Kousha Talebian <kousha.talebian@gmail.com>
2021-06-14 19:59:52 -0700

    lint

--- packages/flex-plugin-e2e-tests/src/utils/assertion.ts
+++ packages/flex-plugin-e2e-tests/src/utils/assertion.ts
@@ -16,23 +16,29 @@ const _strictEqual = (doesEqual: boolean, actual: any, expected: any, msg?: stri
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const equal = (doesEqual: boolean) => (actual: any, expected: any, msg?: string): void => {
-  _strictEqual(doesEqual, actual, expected, msg);
-};
+const equal =
+  (doesEqual: boolean) =>
+  (actual: any, expected: any, msg?: string): void => {
+    _strictEqual(doesEqual, actual, expected, msg);
+  };
 
 /**
  * Checks whether a file/directory exists
  */
-const fileExists = (doesEqual: boolean) => (paths: string[], msg?: string): void => {
-  _strictEqual(doesEqual, true, fs.existsSync(path.join(...paths)), msg);
-};
+const fileExists =
+  (doesEqual: boolean) =>
+  (paths: string[], msg?: string): void => {
+    _strictEqual(doesEqual, true, fs.existsSync(path.join(...paths)), msg);
+  };
 
 /**
  * Checks whether the JSON file contains the given key:value pair
  */
-const jsonFileContains = (doesEqual: boolean) => <T>(paths: string[], key: string, value: T, msg?: string) => {
-  _strictEqual(doesEqual, value, get(JSON.parse(fs.readFileSync(path.join(...paths), 'utf-8')), key), msg);
-};
+const jsonFileContains =
+  (doesEqual: boolean) =>
+  <T>(paths: string[], key: string, value: T, msg?: string) => {
+    _strictEqual(doesEqual, value, get(JSON.parse(fs.readFileSync(path.join(...paths), 'utf-8')), key), msg);
+  };
 
 const stringContains = (doesEqual: boolean) => (line: string, str: string, msg?: string) => {
   _strictEqual(doesEqual, true, line.includes(str), msg);
