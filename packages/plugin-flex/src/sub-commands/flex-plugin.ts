29e232cc1e0bb96fa9a0798118412270fd20a540
Kousha Talebian <kousha.talebian@gmail.com>
2021-07-09 10:34:33 -0700

    Update flex-plugin.ts

--- packages/plugin-flex/src/sub-commands/flex-plugin.ts
+++ packages/plugin-flex/src/sub-commands/flex-plugin.ts
@@ -185,8 +185,9 @@ export default class FlexPlugin extends baseCommands.TwilioClientCommand {
     this.exit = exit;
 
     const doubleDashIndex = argv.indexOf('--');
-    this.internalScriptArgs = doubleDashIndex === -1 ? [] : argv.slice(doubleDashIndex + 1);
-    if (doubleDashIndex !== -1) {
+    const hasInternalArgs = doubleDashIndex === -1;
+    this.internalScriptArgs = hasInternalArgs ? [] : argv.slice(doubleDashIndex + 1);
+    if (hasInternalArgs) {
       process.argv = process.argv.slice(0, doubleDashIndex);
       this.argv = argv.slice(0, doubleDashIndex);
     }
