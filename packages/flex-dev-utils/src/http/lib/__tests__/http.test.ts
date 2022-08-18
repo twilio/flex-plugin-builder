5748621a23cd97648002d6dc827fee49df4ee436
Kousha Talebian <kousha.talebian@gmail.com>
2022-08-18 07:59:31 -0700

    Update packages/flex-dev-utils/src/http/lib/__tests__/http.test.ts
    Co-authored-by: Alice Cai <ahcai@berkeley.edu>

--- packages/flex-dev-utils/src/http/lib/__tests__/http.test.ts
+++ packages/flex-dev-utils/src/http/lib/__tests__/http.test.ts
@@ -106,7 +106,7 @@ describe('HttpClient', () => {
       }
     });
 
-    it('should throw getBaseUrl', () => {
+    it('should return the correct stage urls, () => {
       jest.spyOn(env, 'getRegion').mockReturnValue('stage');
       const urls = [
         ['https://api.twilio.com', 'https://api.stage.twilio.com'],
