9e00be6877c3fa6911b560c8a41dadbc48ebe535
Aishwarya Bannimatti <abannimatti@twilio.com>
2022-09-16 12:08:13 +0530

    Improved code coverage

--- packages/plugin-flex/src/__tests__/commands/flex/plugins/describe/plugin.test.ts
+++ packages/plugin-flex/src/__tests__/commands/flex/plugins/describe/plugin.test.ts
@@ -1,11 +1,30 @@
+import { TwilioCliError } from '@twilio/flex-dev-utils';
+
 import FlexPluginsDescribePlugin from '../../../../../commands/flex/plugins/describe/plugin';
 import FlexPlugin from '../../../../../sub-commands/flex-plugin';
 import InformationFlexPlugin from '../../../../../sub-commands/information-flex-plugin';
-import '../../../../framework';
+import createTest from '../../../../framework';
 
 describe('Commands/Describe/FlexPluginsDescribePlugin', () => {
   it('should have own flags', () => {
     expect(FlexPluginsDescribePlugin.flags).not.toBeSameObject(InformationFlexPlugin.flags);
     expect(FlexPluginsDescribePlugin.flags).not.toBeSameObject(FlexPlugin.flags);
+  });
+
+  it('should set parsed flags', async () => {
+    const cmd = await createTest(FlexPluginsDescribePlugin)('--name', 'plugin-one');
+    await cmd.init();
+    expect(cmd._flags).toBeDefined();
+  });
+
+  it('should throw error if command init not called', async (done) => {
+    const cmd = await createTest(FlexPluginsDescribePlugin)('--name', 'plugin-one');
+    try {
+      await cmd.run();
+    } catch (e) {
+      expect(e instanceof TwilioCliError).toEqual(true);
+      expect(e.message).toContain('Flags are not parsed yet');
+      done();
+    }
   });
 });
