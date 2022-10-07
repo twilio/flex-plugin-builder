5bb79e09941d8b71ef39baa489aa1179e8c2efb6
Aishwarya Bannimatti <abannimatti@twilio.com>
2022-10-07 17:41:25 +0530

    Debugging e2e tests

--- packages/flex-dev-utils/src/progress/lib/progress.ts
+++ packages/flex-dev-utils/src/progress/lib/progress.ts
@@ -66,7 +66,13 @@ export const _getSpinner = (text: string, disabled: boolean): Progress => {
  * @param disabled force enable the progress
  */
 export const progress = async <R>(title: string, action: Callback<R>, disabled = env.isQuiet()): Promise<R> => {
-  const spinner = _getSpinner(logger.markdown(title) || '', disabled);
+  let text: string = '';
+  try {
+    text = logger.markdown(title) || '';
+  } catch (e) {
+    // Ignore error
+  }
+  const spinner = _getSpinner(text, disabled);
 
   try {
     spinner.start();
