ccc5631fb39dcbac95b09578a065f2e07c1e0c1c
hsingla005 <hsingla@twilio.com>
2023-05-18 22:51:27 +0530

    FLEXY-4415 E2E fix for TS template

--- packages/create-flex-plugin/templates/ts2/src/components/CustomTaskList/CustomTaskList.tsx
+++ packages/create-flex-plugin/templates/ts2/src/components/CustomTaskList/CustomTaskList.tsx
@@ -15,7 +15,7 @@ const CustomTaskList = (): JSX.Element | null => {
   return (
     <Theme.Provider theme="default">
       <Alert onDismiss={dismiss} variant="neutral">
-        <Text>
+        <Text as="div">
           This is a dismissible demo component.
         </Text>
       </Alert>
