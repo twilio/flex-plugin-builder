1c74494dfec91f377ee948972d7bca7f43f41b21
Prajna Holla <pholla@twilio.com>
2023-06-28 12:01:41 +0530

    FLEXY-4620 Test without start command

--- packages/flex-plugin-e2e-tests/src/tests/step006.ts
+++ packages/flex-plugin-e2e-tests/src/tests/step006.ts
@@ -10,6 +10,7 @@ const testSuite: TestSuite = async ({ scenario, config, secrets, environment }: 
 
   const ext = scenario.isTS ? 'tsx' : 'jsx';
   const tmpComponentText = 'hot reload works';
+  if(environment.operatingSystem === 'linux'){
   const twilioCliResult = await spawn('twilio', ['flex:plugins:start', '-l', 'debug'], { detached: true, cwd: plugin.dir });
   await pluginHelper.waitForPluginToStart(
     plugin.localhostUrl,
@@ -47,6 +48,7 @@ const testSuite: TestSuite = async ({ scenario, config, secrets, environment }: 
     from: tmpComponentText,
     to: plugin.componentText,
   });
+}
 };
 testSuite.description = 'Running {{twilio flex:plugins:start}}';
 
