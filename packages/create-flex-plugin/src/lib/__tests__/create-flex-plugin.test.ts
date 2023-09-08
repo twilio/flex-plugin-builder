0d6fb6358a8ca8dfa3b0d67c331bd2cf171cbb6f
shyamasish-twilio <shmohanty@twilio.com>
2023-09-08 12:27:52 +0530

    FLEXY-3002 updated test

--- packages/create-flex-plugin/src/lib/__tests__/create-flex-plugin.test.ts
+++ packages/create-flex-plugin/src/lib/__tests__/create-flex-plugin.test.ts
@@ -159,7 +159,7 @@ describe('create-flex-plugin', () => {
       expect(downloadFromGitHub).not.toHaveBeenCalled();
       expect(copyTemplateDir).toHaveBeenCalledTimes(2);
       expect(copyTemplateDir).toHaveBeenCalledWith(
-        expect.stringContaining('templates/ts'),
+        expect.toMatchPathContaining('templates/ts'),
         expect.anything(),
         expect.anything(),
       );
@@ -206,7 +206,7 @@ describe('create-flex-plugin', () => {
       expect(downloadFromGitHub).not.toHaveBeenCalled();
       expect(copyTemplateDir).toHaveBeenCalledTimes(2);
       expect(copyTemplateDir).toHaveBeenCalledWith(
-        expect.stringContaining('templates/ts'),
+        expect.toMatchPathContaining('templates/ts'),
         expect.anything(),
         expect.anything(),
       );
@@ -230,7 +230,7 @@ describe('create-flex-plugin', () => {
       expect(downloadFromGitHub).not.toHaveBeenCalled();
       expect(copyTemplateDir).toHaveBeenCalledTimes(2);
       expect(copyTemplateDir).toHaveBeenCalledWith(
-        expect.stringContaining('templates/js'),
+        expect.toMatchPathContaining('templates/js'),
         expect.anything(),
         expect.anything(),
       );
