f3a65d526aeda09d388e4899d79dadf756907d87
Aishwarya Bannimatti <abannimatti@twilio.com>
2024-01-31 11:25:51 +0530

    FLEXY-4982 revert to node16 and revert pluginServer changes

--- packages/flex-plugin-webpack/src/devServer/pluginServer.ts
+++ packages/flex-plugin-webpack/src/devServer/pluginServer.ts
@@ -1,5 +1,5 @@
 import https from 'https';
-import dns from 'dns';
+// import dns from 'dns';
 
 import { logger, FlexPluginError, exit, env } from '@twilio/flex-dev-utils';
 import { Request, Response } from 'express-serve-static-core';
@@ -257,21 +257,23 @@ export default (
   onRemotePlugin: OnRemotePlugins,
   pluginsConfig: PluginsConfig,
 ): void => {
-  // Fix for ECONNREFUSED
-  dns.setDefaultResultOrder('ipv4first');
+  /*
+   * Fix for ECONNREFUSED
+   * dns.setDefaultResultOrder('ipv4first');
+   */
 
   serverConfig.port = webpackConfig.port || 3000;
 
   webpackConfig.proxy = plugins.local.reduce((proxy, name) => {
     proxy[`/plugins/${name}.js`] = {
-      target: `http://127.0.0.1:${serverConfig.port}`, // placeholder
+      target: `http://localhost:${serverConfig.port}`, // placeholder
       router: () => {
         const match = pluginsConfig[name];
         if (!match) {
           throw new Error();
         }
 
-        return `http://127.0.0.1:${match.port}`;
+        return `http://localhost:${match.port}`;
       },
     };
 
