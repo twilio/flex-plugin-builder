436e05482c6ad4b3d9b50616fb18cdf900ff903f
saroy <saroy@twilio.com>
2024-04-29 14:55:16 +0530

    Updating hint message

--- packages/flex-plugin-scripts/src/prints/validateSuccessful.ts
+++ packages/flex-plugin-scripts/src/prints/validateSuccessful.ts
@@ -35,7 +35,7 @@ const printWarnings = (issues: Warning[]): void => {
         }
         if (link) {
           logger.newline();
-          logger.log(`More details ${logger.coloredStrings.link(logger.coloredStrings.underline(link))}`);
+          logger.log(`For more details, visit: ${logger.coloredStrings.link(logger.coloredStrings.underline(link))}`);
         }
       }
     }
