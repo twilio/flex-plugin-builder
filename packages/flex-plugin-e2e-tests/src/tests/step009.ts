a2210d93fefc522ee1bfac3c6d7b03c4ef02538e
Aishwarya Bannimatti <abannimatti@twilio.com>
2024-04-29 18:03:52 +0530

    FLEXY-4850 Fix step 9 in e2e

--- packages/flex-plugin-e2e-tests/src/tests/step009.ts
+++ packages/flex-plugin-e2e-tests/src/tests/step009.ts
@@ -21,7 +21,7 @@ const testSuite: TestSuite = async ({ scenario, config }: TestParams): Promise<v
   const plugins = await api.getActivePlugins(release!.configuration_sid);
 
   assertion.stringContains(result.stdout, 'successful');
-  assertion.stringContains(result.stdout, 'Configuration FJ');
+  assertion.stringContains(result.stdout, 'Configuration **FJ');
   assertion.stringContains(result.stdout, 'enabled');
   assertion.equal(1, plugins.plugins.length);
   assertion.equal(plugins.plugins[0].unique_name, plugin.name);
