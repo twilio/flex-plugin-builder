34f3f3feb03af8d81cc4ac35410f1937db1ba4cc
sanand <sanand@twilio.com>
2024-09-24 19:07:55 +0530

    test macos

--- packages/flex-plugin-e2e-tests/src/utils/pages/view/base.ts
+++ packages/flex-plugin-e2e-tests/src/utils/pages/view/base.ts
@@ -19,11 +19,35 @@ export abstract class Base {
    */
   protected async goto({ baseUrl, path }: { baseUrl: string; path?: string }): Promise<void> {
     const fullPath = path ? `${baseUrl}/${path}` : baseUrl;
-    const data = await this.page.goto(fullPath, {
-      waitUntil: 'domcontentloaded',
-      timeout: Base.DEFAULT_PAGE_LOAD_TIMEOUT,
-    });
-    logger.info('Page navigate', data?.text());
+    let attempts = 0;
+    const maxAttempts = 3;
+
+    while (attempts < maxAttempts) {
+      try {
+        const data = await this.page.goto(fullPath, {
+          waitUntil: 'domcontentloaded',
+          timeout: Base.DEFAULT_PAGE_LOAD_TIMEOUT,
+        });
+        logger.info('Page navigate', await data?.text());
+        return; // Exit the function if the page loaded successfully
+      } catch (error) {
+        // eslint-disable-next-line no-plusplus
+        attempts++;
+        if (error instanceof Error) {
+          logger.error(`Attempt ${attempts} failed. Error: ${error.message}`);
+        } else {
+          logger.error(`Attempt ${attempts} failed. Unknown error: ${error}`);
+        }
+
+        if (attempts < maxAttempts) {
+          logger.info(`Retrying... Reloading the page (Attempt ${attempts})`);
+          await this.page.reload({ waitUntil: 'domcontentloaded' });
+        } else {
+          logger.error('Maximum attempts reached. Failed to load the page.');
+          throw new Error(`Failed to load page after ${maxAttempts} attempts.`);
+        }
+      }
+    }
   }
 
   /**
