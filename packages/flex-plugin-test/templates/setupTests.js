ff925d91b8b8bdbd8596f2eabeb3bc669f56f2c2
saroy <saroy@twilio.com>
2025-06-12 11:11:51 +0530

    use RTL

--- packages/flex-plugin-test/templates/setupTests.js
+++
@@ -1,8 +0,0 @@
-require('babel-polyfill');
-
-const configure = require('enzyme/build').configure;
-const Adapter = require('enzyme-adapter-react-16/build');
-
-configure({
-  adapter: new Adapter(),
-});
