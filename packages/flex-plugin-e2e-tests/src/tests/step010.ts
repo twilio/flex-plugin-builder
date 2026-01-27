3964fe2e9b5194089a89060f82ddf6712570da08
Pragya Arora <parora@twilio.com>
2026-01-27 17:52:23 +0530

    FLEXY-0000 debug

--- packages/flex-plugin-e2e-tests/src/tests/step010.ts
+++ packages/flex-plugin-e2e-tests/src/tests/step010.ts
@@ -32,7 +32,7 @@ const testSuite: TestSuite = async ({ scenario, config, secrets, environment }: 
   await Browser.create({ flex: config.hostedFlexBaseUrl, twilioConsole: config.consoleBaseUrl });
   // Log into Flex
   await Browser.app.twilioConsole.login('admin', secrets.api.accountSid, config.localhostPort);
-
+  logger.info('>> login success 2');
   await assertion.app.view.adminDashboard.isVisible();
 
   // Verify that user is on the right account
