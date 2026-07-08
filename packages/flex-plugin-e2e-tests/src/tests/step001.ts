9dba0ecd943a0908058d781285c7ea40f31d252a
sanand <sanand@twilio.com>
2026-07-08 17:10:24 +0530

    fix pipeline

--- packages/flex-plugin-e2e-tests/src/tests/step001.ts
+++ packages/flex-plugin-e2e-tests/src/tests/step001.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-unused-modules */
 import { TestSuite, TestParams } from '../core';
 import { spawn } from '../utils';
+import { join } from 'path';
 
 // Install Twilio CLI and Plugins CLI
 const testSuite: TestSuite = async ({ environment, scenario }: TestParams): Promise<void> => {
@@ -13,6 +14,10 @@ const testSuite: TestSuite = async ({ environment, scenario }: TestParams): Prom
   await spawn('npm', cliInstallArgs);
 
   await spawn('twilio', ['plugins:install', `@twilio-labs/plugin-flex@${scenario.packageVersion}`]);
+
+  // Link local packages to replace the installed CLI with local development version
+  const rootDir = join(__dirname, '../../../../..');
+  await spawn('npm', ['run', 'link-packages'], { cwd: rootDir });
 };
 testSuite.description = 'Installing Twilio CLI and Plugins CLI';
 
