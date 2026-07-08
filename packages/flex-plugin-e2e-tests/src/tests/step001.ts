f4e3bea9ba96e86a542f71ea9df221f9d83f0747
sanand <sanand@twilio.com>
2026-07-08 17:55:03 +0530

    fix pipeline

--- packages/flex-plugin-e2e-tests/src/tests/step001.ts
+++ packages/flex-plugin-e2e-tests/src/tests/step001.ts
@@ -1,7 +1,8 @@
 /* eslint-disable import/no-unused-modules */
+import { join } from 'path';
+
 import { TestSuite, TestParams } from '../core';
 import { spawn } from '../utils';
-import { join } from 'path';
 
 // Install Twilio CLI and Plugins CLI
 const testSuite: TestSuite = async ({ environment, scenario }: TestParams): Promise<void> => {
@@ -15,10 +16,13 @@ const testSuite: TestSuite = async ({ environment, scenario }: TestParams): Prom
 
   await spawn('twilio', ['plugins:install', `@twilio-labs/plugin-flex@${scenario.packageVersion}`]);
 
-  // Link local packages to replace the installed CLI with local development version
-  // Run the link-packages.js script directly to avoid workspace context issues
-  const linkPackagesScript = join(environment.cwd, 'link-packages.js');
-  await spawn('node', [linkPackagesScript], { cwd: environment.cwd });
+  /*
+   * Link local packages to replace the installed CLI with local development version
+   * Go up two directories from e2e-tests package to reach repo root
+   */
+  const repoRoot = join(environment.cwd, '..', '..');
+  const linkPackagesScript = join(repoRoot, 'link-packages.js');
+  await spawn('node', [linkPackagesScript], { cwd: repoRoot });
 };
 testSuite.description = 'Installing Twilio CLI and Plugins CLI';
 
